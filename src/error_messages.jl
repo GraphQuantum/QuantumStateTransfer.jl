6a67740001b9a9ed36b9252c7ea072c2fec1a074
Luis-Varona <lbvarona@mta.ca>
2025-05-11 16:09:11 -0300

    Fix typo in licensing headers (mofified -> modified)

--- src/error_messages.jl
+++ src/error_messages.jl
@@ -1,7 +1,7 @@
 # Copyright 2025 Luis M. B. Varona and Nathaniel Johnston
 #
 # Licensed under the MIT license <LICENSE or
-# http://opensource.org/licenses/MIT>. This file may not be copied, mofified, or
+# http://opensource.org/licenses/MIT>. This file may not be copied, modified, or
 # distributed except according to those terms.
 
 const ADJ_MAT_ERR = "Adjacency matrix must be symmetric"
